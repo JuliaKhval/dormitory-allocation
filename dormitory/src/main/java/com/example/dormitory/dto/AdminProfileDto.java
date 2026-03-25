f72d5c7e3ee2f5745d5e02d55f89cba817abc7fd
Julia Khval <juliakhval2512@gmail.com>
2026-03-25 23:38:10 +0300

    delete: files

--- dormitory/src/main/java/com/example/dormitory/dto/AdminProfileDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.dormitory.dto;
-
-import lombok.Data;
-
-@Data
-public class AdminProfileDto {
-    private Long id;
-    private Long userId;
-}
--- dormitory/src/main/java/com/example/dormitory/service/AuthService.java
+++
@@ -1,4 +0,0 @@
-package com.example.dormitory.service;
-
-public class AuthService {
-}
