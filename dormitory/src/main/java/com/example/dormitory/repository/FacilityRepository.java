2e6885af048c94ec0d910fd3ca0d5f5696f26bed
Julia Khval <juliakhval2512@gmail.com>
2026-04-15 00:35:35 +0300

    feat: add FacilityRepository

---
+++ dormitory/src/main/java/com/example/dormitory/repository/FacilityRepository.java
@@ -0,0 +1,12 @@
+package com.example.dormitory.repository;
+
+import com.example.dormitory.entity.Facility;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FacilityRepository extends JpaRepository<Facility, Long> {
+    Optional<Facility> findByName(String name);
+}
